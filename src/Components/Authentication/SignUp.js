5939c8d400dcae23929f3b21145dd289596430e8
Joshua Lee <jdl93@nau.edu>
2018-11-17 08:13:52 -1000

    cannot get signup error

--- src/Components/Authentication/SignUp.js
+++ src/Components/Authentication/SignUp.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Redirect } from "react-router-dom";
+import { signUp } from "../../Store/Actions/authActions";
 
 class SignUp extends Component {
   state = {
@@ -16,10 +17,10 @@ class SignUp extends Component {
   };
   handleSubmit = e => {
     e.preventDefault();
-    console.log(this.state);
+    this.props.signUp(this.state);
   };
   render() {
-    const { auth } = this.props;
+    const { authError, auth } = this.props;
     if (auth.uid) return <Redirect to="/" />;
     return (
       <div className="container section">
@@ -64,8 +65,18 @@ class SignUp extends Component {
 
 const mapStateToProps = state => {
   return {
-    auth: state.firebase.auth
+    auth: state.firebase.auth,
+    authError: state.auth.authError
   };
 };
 
-export default connect(mapStateToProps)(SignUp);
+const mapDispatchToProps = dispatch => {
+  return {
+    signUp: creds => dispatch(signUp(creds))
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(SignUp);
